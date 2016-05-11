e2c379510e24d48ad121e5fe68d7d7b3ba02d9c9
Luannguyen <nkluan.gl@gmail.com>
2016-05-12 02:44:52 +0700

    [update]

---
+++ start8.bat
@@ -0,0 +1,5 @@
+title omo - www.OMO.com
+COLOR 0A
+SET PATH="%CD%"\runtime\pgsql\bin;"%CD%"\runtime\python;"%CD%"\runtime\win32\wkhtmltopdf;"%CD%"\runtime\nodejs;"%CD%"\runtime\nodejs\lessc.cmd;%PATH%.
+"%CD%"\runtime\pgsql\bin\pg_ctl -D "%CD%"\runtime\pgsql\data -l "%CD%"\runtime\pgsql\logfile start
+"%CD%"\runtime\python\python-oe "%CD%"\..\Gold-server-8\odoo\openerp-server -c "%CD%"\Gold-server-8.conf
